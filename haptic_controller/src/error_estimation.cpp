0e94cb8780c26cfda8ff7511039a1fa18ec30bec
Luis Maldonado <lfmaldon@ens2m.org>
2024-04-16 12:17:46 +0200

    changed error definition from distance to angular error

--- haptic_controller/src/error_estimation.cpp
+++ haptic_controller/src/error_estimation.cpp
@@ -3,10 +3,12 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "visualization_msgs/msg/marker.hpp"
+#include "visualization_msgs/msg/interactive_marker_feedback.hpp"
 #include "geometry_msgs/msg/point.hpp"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include <Eigen/Dense>
 #include <Eigen/Geometry> 
 
@@ -15,8 +17,8 @@ class ErrorEstimation : public rclcpp::Node
 public:
     ErrorEstimation() : Node("error_estimation"), tf_buffer_(this->get_clock()), tf_listener_(tf_buffer_)
     {
-        marker_sub_ = this->create_subscription<visualization_msgs::msg::Marker>(
-            "/needle_trajectory", 10, std::bind(&ErrorEstimation::marker_callback, this, std::placeholders::_1));
+        int_marker_sub_ = this->create_subscription<visualization_msgs::msg::InteractiveMarkerFeedback>(
+            "/trajectory_marker/feedback", 10, std::bind(&ErrorEstimation::marker_callback, this, std::placeholders::_1));
 
         // Timer to trigger error calculation and logging every second (only for testing)
         timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&ErrorEstimation::calculate_and_log_error, this));
@@ -26,39 +28,22 @@ public:
 
 private:
 
-    void marker_callback(const visualization_msgs::msg::Marker::SharedPtr marker_msg)
+    void marker_callback(const visualization_msgs::msg::InteractiveMarkerFeedback::SharedPtr msg)
     {
-
-        if (marker_msg->type == visualization_msgs::msg::Marker::LINE_STRIP)
+        if (msg->event_type == visualization_msgs::msg::InteractiveMarkerFeedback::POSE_UPDATE)
         {
-            // last_marker_position_ = marker_msg->pose.position;
-            last_marker_position_ = marker_msg->points[1];
-            
-            // RCLCPP_INFO(this->get_logger(), "Received marker at position: (%.2f, %.2f, %.2f)",
-            //             last_marker_position_.x, last_marker_position_.y, last_marker_position_.z);
-            // calculate_and_log_error();
+            // Extract position information from the feedback message
+            // The marker is an arrow that starts at PI, has an arbitrary length
+            // and its orientaition its defined by the user via an interactive marker
+            auto arrow_position = msg->pose.position;
+            auto arrow_orientation = msg->pose.orientation;
+
+            // Convert arrow position and orientation to Eigen vectors
+            arrow_start << arrow_position.x, arrow_position.y, arrow_position.z;
+            Eigen::Quaterniond arrow_quaternion(arrow_orientation.w, arrow_orientation.x, arrow_orientation.y, arrow_orientation.z);
+            arrow_end = arrow_start + arrow_quaternion * Eigen::Vector3d(1.0, 0.0, 0.0);
         }
     }
-
-    // void calculate_and_log_error()
-    // {
-    //     try
-    //     {
-    //         auto transform_stamped = tf_buffer_.lookupTransform("world", "needle_interaction_link", tf2::TimePointZero);
-
-    //         // Calculate the error
-    //         auto error_x = last_marker_position_.x - transform_stamped.transform.translation.x;
-    //         auto error_y = last_marker_position_.y - transform_stamped.transform.translation.y;
-    //         auto error_z = last_marker_position_.z - transform_stamped.transform.translation.z;
-    //         double magnitude_error = sqrt(pow(error_x, 2) + pow(error_y, 2) + pow(error_z, 2));
-
-    //         RCLCPP_INFO(this->get_logger(), "Error Magnitude: %.2f", magnitude_error);
-    //     }
-    //     catch (tf2::TransformException &ex)
-    //     {
-    //         RCLCPP_ERROR(this->get_logger(), "Could not transform from 'world' to 'needle_interaction_link': %s", ex.what());
-    //     }
-    // }
 
     void calculate_and_log_error()
     {
@@ -68,26 +53,19 @@ private:
 
             // Extract needle interaction point (PU) position
             auto PU = transform_stamped.transform.translation;
+            Eigen::Vector3d PU_vec(PU.x, PU.y, PU.z);
 
-            // Define vectors from insertion point (PI) to marker point and PU
+            // Vector from insertion point (PI) to arrow tip 
             // Vectors are defined in PI frame
-            Eigen::Vector3d marker_vec(last_marker_position_.x - PI_x, 
-                    last_marker_position_.y - PI_y, 
-                    last_marker_position_.z - PI_z);
+            Eigen::Vector3d marker_vec = arrow_end - arrow_start;
 
-            Eigen::Vector3d PU_vec(PU.x - PI_x, PU.y - PI_y, PU.z - PI_z);
+            // Vector from PI to PU
+            Eigen::Vector3d PIU_vec = PU_vec - arrow_start;
 
-            // Direction vector of the line from insertion point to marker point
-            Eigen::Vector3d line_dir = marker_vec.normalized();
+            // Calculate the angle between the arrow vector and the vector from the fixed point to the third point
+            double angle_error = std::acos(marker_vec.normalized().dot(PIU_vec.normalized()));
 
-            // Point on the line closest to end effector (projection)
-            double t = PU_vec.dot(line_dir);
-            Eigen::Vector3d closest_point = t * line_dir;
-
-            // Calculate distance between end effector point and closest point on the line
-            double magnitude_error = (PU_vec - closest_point).norm();
-
-            RCLCPP_INFO(this->get_logger(), "Distance between needle and trajectory: %.2f", magnitude_error);
+            RCLCPP_INFO(this->get_logger(), "Angular error between needle and trajectory: %.2f", angle_error);
         }
         catch (tf2::TransformException &ex)
         {
@@ -95,8 +73,12 @@ private:
         }
     }
 
-    geometry_msgs::msg::Point_<std::allocator<void>> last_marker_position_;
-    rclcpp::Subscription<visualization_msgs::msg::Marker>::SharedPtr marker_sub_;
+    // geometry_msgs::msg::Point_<std::allocator<void>> last_marker_position_;
+    Eigen::Vector3d last_marker_position_;
+    Eigen::Vector3d arrow_start;
+    Eigen::Vector3d arrow_end;
+    rclcpp::Subscription<visualization_msgs::msg::InteractiveMarkerFeedback>::SharedPtr int_marker_sub_;
+
     tf2_ros::Buffer tf_buffer_;
     tf2_ros::TransformListener tf_listener_;
     rclcpp::TimerBase::SharedPtr timer_;
